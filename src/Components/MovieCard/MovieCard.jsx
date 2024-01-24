9810cc6b9d2f4acc947e98f39db2d72ebba95dc5
Ray <buttrafay980@gmail.com>
2024-01-24 20:04:15 +0500

    updatin

--- src/Components/MovieCard/MovieCard.jsx
+++ src/Components/MovieCard/MovieCard.jsx
@@ -2,72 +2,74 @@ import { memo } from "react";
 import Styles from "./MovieCard.module.css";
 import { ReactComponent as Star } from "../../Assets/Imgs/Star.svg";
 import { ReactComponent as Language } from "../../Assets/Imgs/language.svg";
+import { ReactComponent as Download } from "../../Assets/Imgs/download.svg";
 // import { ReactComponent as Bookmark } from '../../Assets/Imgs/bookmark.svg';
 import { ReactComponent as Play } from "../../Assets/Imgs/play.svg";
 import { motion } from "framer-motion";
 
-const MovieCard = ({ movie, variants }) => {
+const MovieCard = ( { movie, variants } ) => {
   return (
     <motion.a
-      href={`https://www.imdb.com/title/${movie.imdb_code}`}
-      className={Styles.cover}
+      href={ `https://www.imdb.com/title/${ movie.imdb_code }` }
+      className={ Styles.cover }
       target="_blank"
-      variants={variants}
+      variants={ variants }
     >
       <motion.div
-        className={Styles.card}
-        transition={{
+        className={ Styles.card }
+        transition={ {
           delay: 0,
           duration: 0.2,
           type: "spring",
           damping: 9,
-        }}
-        whileHover={{ scale: 1.04 }}
+        } }
+        whileHover={ { scale: 1.04 } }
       >
-        <div className={Styles.image}>
-          <img src={movie["large_cover_image"]} alt="" />
-          <div className={Styles.torrents}>
-            {movie?.torrents.map((torrent, index) => (
-              <a href={torrent.url} target="_blank" className={Styles.torrent}>
-                {torrent.quality}
+        <div className={ Styles.image }>
+          <img src={ movie[ "large_cover_image" ] } alt="" />
+          <div className={ Styles.torrents }>
+            { movie?.torrents.map( ( torrent, index ) => (
+              <a href={ torrent.url } target="_blank" className={ Styles.torrent }>
+                <Download className={ Styles.download } />
+                { torrent.quality }
               </a>
-            ))}
+            ) ) }
           </div>
         </div>
-        <div className={Styles.content}>
-          <div className={Styles["title-rating-genre"]}>
-            <div className={Styles["title-rating"]}>
-              <p className={Styles["title"]}>{movie.title.slice(0, 20)}...</p>
-              <div className={Styles["rating"]}>
-                <Star className={Styles["star"]} />
-                <span>{`${movie.rating} / 10`}</span>
+        <div className={ Styles.content }>
+          <div className={ Styles[ "title-rating-genre" ] }>
+            <div className={ Styles[ "title-rating" ] }>
+              <p className={ Styles[ "title" ] }>{ movie.title.slice( 0, 20 ) }...</p>
+              <div className={ Styles[ "rating" ] }>
+                <Star className={ Styles[ "star" ] } />
+                <span>{ `${ movie.rating } / 10` }</span>
               </div>
             </div>
-            <div className={Styles["year-genre"]}>
-              <p className={Styles["year"]}>{movie.year}.</p>
-              <p className={Styles["genre"]}>{movie.genres.join(", ")}</p>
+            <div className={ Styles[ "year-genre" ] }>
+              <p className={ Styles[ "year" ] }>{ movie.year }.</p>
+              <p className={ Styles[ "genre" ] }>{ movie.genres.join( ", " ) }</p>
             </div>
           </div>
-          <div className={Styles["summary-lang"]}>
-            <div className={Styles["headings"]}>
-              <p className={Styles["heading"]}>Summary</p>
-              <div className={Styles["lang"]}>
-                <Language className={Styles["lang"]} />{" "}
-                <span>{movie.language}</span>
+          <div className={ Styles[ "summary-lang" ] }>
+            <div className={ Styles[ "headings" ] }>
+              <p className={ Styles[ "heading" ] }>Summary</p>
+              <div className={ Styles[ "lang" ] }>
+                <Language className={ Styles[ "lang" ] } />{ " " }
+                <span>{ movie.language }</span>
               </div>
             </div>
-            <p className={Styles["desc"]}>
-              {movie.summary.slice(0, 100) + "..."}
+            <p className={ Styles[ "desc" ] }>
+              { movie.summary.slice( 0, 100 ) + "..." }
             </p>
           </div>
-          <div className={Styles["trailer-bookmark"]}>
+          <div className={ Styles[ "trailer-bookmark" ] }>
             <a
-              onClick={(e) => e.stopPropagation()}
+              onClick={ ( e ) => e.stopPropagation() }
               target="_blank"
-              href={`https://www.youtube.com/embed/${movie["yt_trailer_code"]}?rel=0&wmode=transparent&border=0&autoplay=1&iv_load_policy=3`}
-              className={Styles.play}
+              href={ `https://www.youtube.com/embed/${ movie[ "yt_trailer_code" ] }?rel=0&wmode=transparent&border=0&autoplay=1&iv_load_policy=3` }
+              className={ Styles.play }
             >
-              <Play className={Styles.play} />
+              <Play className={ Styles.play } />
               <span>Watch Trailer</span>
             </a>
             {/* <button type='button' className={ Styles.bm }>
@@ -80,4 +82,4 @@ const MovieCard = ({ movie, variants }) => {
   );
 };
 
-export default memo(MovieCard);
+export default memo( MovieCard );
--- src/Pages/SearchPage/SearchPage.jsx
+++ src/Pages/SearchPage/SearchPage.jsx
@@ -4,40 +4,40 @@ import { AnimatePresence, motion } from "framer-motion";
 import MovieCardContainer from "../../Components/MovieCardContainer/MovieCardContainer";
 // import MG from "../../Assets/Imgs/magnifying_glass.svg";
 
-const Inputs = memo(({ input, setInput, handleSearch = () => {} }) => {
+const Inputs = memo( ( { input, setInput, handleSearch = () => { } } ) => {
   return (
-    <div className={Styles.inputs} onKeyDown={handleSearch}>
+    <div className={ Styles.inputs } onKeyDown={ handleSearch }>
       <input
         type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
+        value={ input }
+        onChange={ ( e ) => setInput( e.target.value ) }
         placeholder="Search Movies"
       />
       <motion.button
-        whileHover={{ width: "70px" }}
-        whileTap={{ scale: 0.8 }}
+        whileHover={ { width: "70px" } }
+        whileTap={ { scale: 0.8 } }
         type="button"
-        onClick={() => handleSearch({ key: "Enter" })}
+        onClick={ () => handleSearch( { key: "Enter" } ) }
       />
     </div>
   );
-});
+} );
 
 const SearchPage = () => {
-  const [input, setInput] = useState("");
-  const [movies, setMovies] = useState([]);
-  const [pending, setPending] = useState(false);
-  const [fetchMore, setFetchMore] = useState(false);
+  const [ input, setInput ] = useState( "" );
+  const [ movies, setMovies ] = useState( [] );
+  const [ pending, setPending ] = useState( false );
+  const [ fetchMore, setFetchMore ] = useState( false );
 
-  async function fetchMovies({ Controller }) {
-    if (!input.trim().length) {
-      setMovies([]);
+  async function fetchMovies ( { Controller } ) {
+    if ( !input.trim().length ) {
+      setMovies( [] );
       return;
     }
 
     try {
       const reponse = await fetch(
-        `https://yts.mx/api/v2/list_movies.json?query_term=${input}`,
+        `https://yts.mx/api/v2/list_movies.json?query_term=${ input }`,
         {
           signal: Controller?.signal,
         }
@@ -45,33 +45,32 @@ const SearchPage = () => {
 
       const body = await reponse.json();
 
-      if (reponse.ok) {
-        if (body.data?.movie_count > 0) {
-          setMovies(body.data.movies);
-          setPending(body.data?.movie_count - 20 > 0 ? true : false);
+      if ( reponse.ok ) {
+        if ( body.data?.movie_count > 0 ) {
+          setMovies( body.data.movies );
+          setPending( body.data?.movie_count - 20 > 0 ? true : false );
         } else {
-          setMovies([]);
+          setMovies( [] );
         }
       }
 
-      console.log(body);
-    } catch (e) {
-      if (!Controller?.signal.aborted) {
-        console.log(e);
+      console.log( body );
+    } catch ( e ) {
+      if ( !Controller?.signal.aborted ) {
+        console.log( e );
       }
     }
   }
 
-  async function fetchPage({ page, Controller }) {
-    if (!input.trim().length) {
-      setMovies([]);
+  async function fetchPage ( { page, Controller } ) {
+    if ( !input.trim().length ) {
+      setMovies( [] );
       return;
     }
 
     try {
       const reponse = await fetch(
-        `https://yts.mx/api/v2/list_movies.json?query_term=${input}${
-          page ? `&page=${page}` : ""
+        `https://yts.mx/api/v2/list_movies.json?query_term=${ input }${ page ? `&page=${ page }` : ""
         }`,
         {
           signal: Controller?.signal,
@@ -80,78 +79,78 @@ const SearchPage = () => {
 
       const body = await reponse.json();
 
-      if (reponse.ok) {
-        if (body.data?.movie_count - movies.length > 0) {
-          setMovies((prev) => [...prev, ...body.data.movies]);
-          setPending(true);
+      if ( reponse.ok ) {
+        if ( body.data?.movie_count - movies.length > 0 ) {
+          setMovies( ( prev ) => [ ...prev, ...body.data.movies ] );
+          setPending( true );
         } else {
-          setPending(false);
+          setPending( false );
         }
       }
 
-      console.log(body);
-    } catch (e) {
-      if (!Controller?.signal.aborted) {
-        console.log(e);
+      console.log( body );
+    } catch ( e ) {
+      if ( !Controller?.signal.aborted ) {
+        console.log( e );
       }
     } finally {
-      setFetchMore(false);
+      setFetchMore( false );
     }
   }
 
-  useEffect(() => {
+  useEffect( () => {
     const Controller = new AbortController();
 
-    fetchMovies({ Controller });
+    fetchMovies( { Controller } );
 
     return () => Controller.abort();
-  }, [input]);
+  }, [ input ] );
 
-  useEffect(() => {
+  useEffect( () => {
     const Controller = new AbortController();
 
-    if (fetchMore && movies.length) {
-      fetchPage({ Controller, page: movies.length / 20 + 1 });
+    if ( fetchMore && movies.length ) {
+      fetchPage( { Controller, page: movies.length / 20 + 1 } );
     }
 
     return () => Controller.abort();
-  }, [fetchMore]);
+  }, [ fetchMore ] );
 
   return (
     // <AnimatePresence mode='popLayout'>
     <>
-      <div className={Styles.overlay}></div>
-      <div className={Styles.container}>
-        <div className={Styles.hero}>
-          <p className={Styles.punchline}>Download Movies: HD smallest size</p>
+      <div className={ Styles.overlay }></div>
+      <div className={ Styles.container }>
+        <div className={ Styles.hero }>
+          <p className={ Styles.punchline }>Download Movies: HD smallest size</p>
           <Inputs
-            input={input}
-            setInput={setInput}
-            handleSearch={(e) =>
-              e.key == "Enter" && fetchMovies({ Controller: null })
+            input={ input }
+            setInput={ setInput }
+            handleSearch={ ( e ) =>
+              e.key == "Enter" && fetchMovies( { Controller: null } )
             }
           />
-          <p className={Styles.note}>
+          <p className={ Styles.note }>
             Here you can browse and download movies in excellent 720p, 1080p,
             2160p 4K and 3D quality, all at the smallest file size.
           </p>
-          {/* YTS Movies Torrents. */}
+          {/* YTS Movies Torrents. */ }
         </div>
-        <div className={Styles.movies}>
-          <MovieCardContainer movies={movies} />
-          {pending && input.trim().length && (
+        <div className={ Styles.movies }>
+          <MovieCardContainer movies={ movies } />
+          { pending && input.trim().length && (
             <motion.button
-              whileHover={{ filter: "saturate(.7)", scale: 0.9 }}
-              transition={{ type: "spring" }}
+              whileHover={ { filter: "saturate(.7)", scale: 0.9 } }
+              transition={ { type: "spring" } }
               type="button"
-              className={Styles.load_more}
-              onClick={() => {
-                setFetchMore(true);
-              }}
+              className={ Styles.load_more }
+              onClick={ () => {
+                setFetchMore( true );
+              } }
             >
               Load More
             </motion.button>
-          )}
+          ) }
         </div>
       </div>
     </>
@@ -159,4 +158,4 @@ const SearchPage = () => {
   );
 };
 
-export default memo(SearchPage);
+export default memo( SearchPage );
