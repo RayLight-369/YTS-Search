8a0adbe0653d7c9d2d27b60b37d2096101b26a36
AIONS <146433246+aionstech@users.noreply.github.com>
2024-01-26 11:18:50 +0500

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = ( { handleRequest } ) => {
             height={ 75 }
             alt='footer'
           />
-          <a className={ styles[ "email" ] } >S</a> {/*"href='mailto:help@aions.co'"*/ }
+          <a className={ styles[ "email" ] } >Stooge Pictures</a> {/*"href='mailto:help@aions.co'"*/ }
           
         </div>
         <div className={ styles[ "update" ] }>
