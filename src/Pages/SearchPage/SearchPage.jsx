24503ea0bf84c7f40eca2ea64b60f9290239d4ba
RayLight-369 <abdulrafay.designs@gmail.com>
2024-01-29 20:43:08 +0500

    updating

--- src/Pages/SearchPage/SearchPage.jsx
+++ src/Pages/SearchPage/SearchPage.jsx
@@ -5,37 +5,54 @@ import MovieCardContainer from "../../Components/MovieCardContainer/MovieCardCon
 import Modal from "../../Components/Modal/Modal";
 import RequestForm from "../../Components/RequestForm/RequestForm";
 import Footer from "../../Components/Footer/Footer";
+// import torrentStream from "torrent-stream";
+// import { pipeline } from "stream";
 // import MG from "../../Assets/Imgs/magnifying_glass.svg";
 
-
-const Inputs = memo( ( { input, setInput, handleSearch = () => { } } ) => {
+const Inputs = memo(({ input, setInput, handleSearch = () => {} }) => {
   return (
-    <div className={ Styles.inputs } onKeyDown={ handleSearch }>
+    <div className={Styles.inputs} onKeyDown={handleSearch}>
       <input
         type="text"
-        value={ input }
-        onChange={ ( e ) => setInput( e.target.value ) }
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
         placeholder="Search Movies"
       />
       <motion.button
-        whileHover={ { width: "70px" } }
-        whileTap={ { scale: 0.8 } }
+        whileHover={{ width: "70px" }}
+        whileTap={{ scale: 0.8 }}
         type="button"
-        onClick={ () => handleSearch( { key: "Enter" } ) }
+        onClick={() => handleSearch({ key: "Enter" })}
       />
     </div>
   );
-} );
+});
 
 const SearchPage = () => {
-  const [ input, setInput ] = useState( "" );
-  const [ movies, setMovies ] = useState( [] );
-  const [ pending, setPending ] = useState( false );
-  const [ fetchMore, setFetchMore ] = useState( false );
-  const [ trailer, setTrailer ] = useState( { show: false, src: "" } );
-  const [ showRequest, setShowRequest ] = useState( false );
+  const [input, setInput] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [pending, setPending] = useState(false);
+  const [fetchMore, setFetchMore] = useState(false);
+  const [trailer, setTrailer] = useState({ show: false, src: "" });
+  const [showRequest, setShowRequest] = useState(false);
+  const [showPlayer, setShowPlayer] = useState({ show: false, hash: "" });
 
-  async function fetchMovies ( { Controller } ) {
+  useEffect(() => {
+    const script = document.createElement("script");
+
+    script.src =
+      "https://cdn.jsdelivr.net/npm/@webtor/embed-sdk-js/dist/index.min.js";
+    script.async = true;
+    script.type = "text/javascript";
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  async function fetchMovies({ Controller }) {
     // if ( !input.trim().length ) {
     //   await fetch(
     //     `https://yts.mx/api/v2/list_movies.json?limit=6`,
@@ -48,7 +65,7 @@ const SearchPage = () => {
 
     try {
       const reponse = await fetch(
-        `https://yts.mx/api/v2/list_movies.json?query_term=${ input }`,
+        `https://yts.mx/api/v2/list_movies.json?query_term=${input}`,
         {
           signal: Controller?.signal,
         }
@@ -56,32 +73,33 @@ const SearchPage = () => {
 
       const body = await reponse.json();
 
-      if ( reponse.ok ) {
-        if ( body.data?.movie_count > 0 ) {
-          setMovies( body.data.movies );
-          setPending( body.data?.movie_count - 20 > 0 ? true : false );
+      if (reponse.ok) {
+        if (body.data?.movie_count > 0) {
+          setMovies(body.data.movies);
+          setPending(body.data?.movie_count - 20 > 0 ? true : false);
         } else {
-          setMovies( [] );
+          setMovies([]);
         }
       }
 
-      console.log( body );
-    } catch ( e ) {
-      if ( !Controller?.signal.aborted ) {
-        console.log( e );
+      console.log(body);
+    } catch (e) {
+      if (!Controller?.signal.aborted) {
+        console.log(e);
       }
     }
   }
 
-  async function fetchPage ( { page, Controller } ) {
-    if ( !input.trim().length ) {
-      setMovies( [] );
+  async function fetchPage({ page, Controller }) {
+    if (!input.trim().length) {
+      setMovies([]);
       return;
     }
 
     try {
       const reponse = await fetch(
-        `https://yts.mx/api/v2/list_movies.json?query_term=${ input }${ page ? `&page=${ page }` : ""
+        `https://yts.mx/api/v2/list_movies.json?query_term=${input}${
+          page ? `&page=${page}` : ""
         }`,
         {
           signal: Controller?.signal,
@@ -90,99 +108,113 @@ const SearchPage = () => {
 
       const body = await reponse.json();
 
-      if ( reponse.ok ) {
-        if ( body.data?.movie_count - movies.length > 0 ) {
-          setMovies( ( prev ) => [ ...prev, ...body.data.movies ] );
-          setPending( true );
+      if (reponse.ok) {
+        if (body.data?.movie_count - movies.length > 0) {
+          setMovies((prev) => [...prev, ...body.data.movies]);
+          setPending(true);
         } else {
-          setPending( false );
+          setPending(false);
         }
       }
 
-      console.log( body );
-    } catch ( e ) {
-      if ( !Controller?.signal.aborted ) {
-        console.log( e );
+      console.log(body);
+    } catch (e) {
+      if (!Controller?.signal.aborted) {
+        console.log(e);
       }
     } finally {
-      setFetchMore( false );
+      setFetchMore(false);
     }
   }
 
-  useEffect( () => {
+  useEffect(() => {
     const Controller = new AbortController();
 
-    fetchMovies( { Controller } );
+    fetchMovies({ Controller });
 
     return () => Controller.abort();
-  }, [ input ] );
+  }, [input]);
 
-  useEffect( () => {
+  useEffect(() => {
     const Controller = new AbortController();
 
-    if ( fetchMore && movies.length ) {
-      fetchPage( { Controller, page: movies.length / 20 + 1 } );
+    if (fetchMore && movies.length) {
+      fetchPage({ Controller, page: movies.length / 20 + 1 });
     }
 
     return () => Controller.abort();
-  }, [ fetchMore ] );
+  }, [fetchMore]);
 
-  const handleClose = () => setShowRequest( false );
+  const handleClose = () => setShowRequest(false);
 
   return (
     // <AnimatePresence mode='popLayout'>
     <>
-      <div className={ Styles.overlay }></div>
+      <div className={Styles.overlay}></div>
       <AnimatePresence mode="wait">
-        { trailer.show && (
-          <Modal handleClose={ () => setTrailer( { show: false, src: "" } ) } customClassName={ Styles.modal }>
-            <iframe src={ trailer.src } width={ 640 } height={ 360 } style={ { border: "none" } } />
+        {trailer.show && (
+          <Modal
+            handleClose={() => setTrailer({ show: false, src: "" })}
+            customClassName={Styles.modal}
+          >
+            <iframe
+              src={trailer.src}
+              width={640}
+              height={360}
+              style={{ border: "none" }}
+            />
           </Modal>
-        ) }
+        )}
       </AnimatePresence>
-      <div className={ Styles.container }>
-        <div className={ Styles.hero }>
-          <p className={ Styles.punchline }>HD Movies With Smallest Size</p>
+      <div className={Styles.container}>
+        <div className={Styles.hero}>
+          <p className={Styles.punchline}>HD Movies With Smallest Size</p>
           <Inputs
-            input={ input }
-            setInput={ setInput }
-            handleSearch={ ( e ) =>
-              e.key == "Enter" && fetchMovies( { Controller: null } )
+            input={input}
+            setInput={setInput}
+            handleSearch={(e) =>
+              e.key == "Enter" && fetchMovies({ Controller: null })
             }
           />
-          <p className={ Styles.note }>
-            Browse Movies in 720p, 1080p, 2K, 4K and 3D quality, All At Smallest file Size.
+          <p className={Styles.note}>
+            Browse Movies in 720p, 1080p, 2K, 4K and 3D quality, All At Smallest
+            file Size.
           </p>
-          {/* SP Movies Torrents. */ }
+          {/* SP Movies Torrents. */}
         </div>
-        <div className={ Styles.movies }>
-          <MovieCardContainer setTrailer={ setTrailer } movies={ movies } />
-          { pending && input.trim().length && (
+        <div className={Styles.movies}>
+          <MovieCardContainer setTrailer={setTrailer} movies={movies} />
+          {pending && input.trim().length && (
             <motion.button
-              whileHover={ { filter: "saturate(.7)", scale: 0.9 } }
-              transition={ { type: "spring" } }
+              whileHover={{ filter: "saturate(.7)", scale: 0.9 }}
+              transition={{ type: "spring" }}
               type="button"
-              className={ Styles.load_more }
-              onClick={ () => {
-                setFetchMore( true );
-              } }
+              className={Styles.load_more}
+              onClick={() => {
+                setFetchMore(true);
+              }}
             >
               Load More
             </motion.button>
-          ) }
+          )}
         </div>
       </div>
-      <Footer handleRequest={ () => setShowRequest( true ) } />
+      <Footer handleRequest={() => setShowRequest(true)} />
       <AnimatePresence mode="wait">
-        { showRequest && (
-          <Modal handleClose={ handleClose }>
-            <RequestForm handleClose={ handleClose } />
+        {showRequest && (
+          <Modal handleClose={handleClose}>
+            <RequestForm handleClose={handleClose} />
           </Modal>
-        ) }
+        )}
+        {showPlayer.show && (
+          <Modal handleClose={handleClose}>
+            <div className="webtor" id="videoplayer"></div>
+          </Modal>
+        )}
       </AnimatePresence>
     </>
     // </AnimatePresence>
   );
 };
 
-export default memo( SearchPage );
+export default memo(SearchPage);
