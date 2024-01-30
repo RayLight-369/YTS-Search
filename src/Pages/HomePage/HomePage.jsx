ba90253f1c40af19dedbd8f823d9cf0ee532919c
RayLight-369 <abdulrafay.designs@gmail.com>
2024-01-30 19:55:43 +0500

    updating image load bg

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -41,58 +41,61 @@ const HomePage = () => {
   return (
     // <AnimatePresence mode='wait'>
     <>
-      {loaded && (
-        <section className={Styles["home-section"]}>
-          <div className={Styles["overlay"]}></div>
-          <motion.div
-            className={Styles["container"]}
-            variants={variants}
-            animate="animate"
-            initial="initial"
-            exit="exit"
-            transition={{ duration: 0.7 }}
-          >
+      <section className={Styles["home-section"]}>
+        <div className={Styles["overlay"]}></div>
+        {loaded && (
+          <>
             <motion.div
-              className={Styles["hero"]}
+              className={Styles["container"]}
               variants={variants}
-              transition={{ delay: 0.2, duration: 0.7 }}
+              animate="animate"
+              initial="initial"
+              exit="exit"
+              transition={{ duration: 0.7 }}
             >
-              <p className={Styles["p1"]}>Stooge Pictures</p>
               <motion.div
-                className={Styles["d2"]}
+                className={Styles["hero"]}
                 variants={variants}
-                transition={{ delay: 0.45, duration: 0.7 }}
+                transition={{ delay: 0.2, duration: 0.7 }}
               >
-                <p className={Styles["description"]}>
-                  Experience seamless entertainment where you can watch movies
-                  and shows, and even download your favorites for offline
-                  enjoyment.
-                </p>
-                <p className={Styles["p2"]}>Feel Free</p>
+                <p className={Styles["p1"]}>Stooge Pictures</p>
+                <motion.div
+                  className={Styles["d2"]}
+                  variants={variants}
+                  transition={{ delay: 0.45, duration: 0.7 }}
+                >
+                  <p className={Styles["description"]}>
+                    Experience seamless entertainment where you can watch movies
+                    and shows, and even download your favorites for offline
+                    enjoyment.
+                  </p>
+                  <p className={Styles["p2"]}>Feel Free</p>
+                </motion.div>
               </motion.div>
             </motion.div>
-          </motion.div>
-          <motion.div
-            className={Styles["links"]}
-            variants={socialLinksVariants}
-            animate="animate"
-            initial="initial"
-            transition={{ staggerChildren: 0.2, duration: 1 }}
-          >
-            {SocialLionks.map((social, index) => (
-              <motion.a
-                href={social.link}
-                target="_blank"
-                className={Styles["link"]}
-                variants={socialLinksVariants}
-                whileHover={{ scale: 1.4 }}
-              >
-                <img src={social.src} alt="" />
-              </motion.a>
-            ))}
-          </motion.div>
-        </section>
-      )}
+            <motion.div
+              className={Styles["links"]}
+              variants={socialLinksVariants}
+              animate="animate"
+              initial="initial"
+              transition={{ staggerChildren: 0.2, duration: 1 }}
+            >
+              {SocialLionks.map((social, index) => (
+                <motion.a
+                  href={social.link}
+                  target="_blank"
+                  className={Styles["link"]}
+                  variants={socialLinksVariants}
+                  whileHover={{ scale: 1.4 }}
+                  key={index}
+                >
+                  <img src={social.src} alt="" />
+                </motion.a>
+              ))}
+            </motion.div>
+          </>
+        )}
+      </section>
     </>
     // </AnimatePresence>
   );
