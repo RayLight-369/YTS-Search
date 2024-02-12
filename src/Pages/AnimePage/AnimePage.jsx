46a18af436ab8d29252be90bacd1772c7e1c1da9
Ray <buttrafay980@gmail.com>
2024-02-12 16:41:50 +0500

    updating

--- src/Pages/AnimePage/AnimePage.jsx
+++ src/Pages/AnimePage/AnimePage.jsx
@@ -10,8 +10,26 @@ const AnimePage = () => {
 
   const [ input, setInput ] = useState( "" );
   const [ searchResults, setSearchResults ] = useState( null );
+  const [ recentEpisodes, setRecentEpisodes ] = useState( null );
 
+  const fetchRecentEpisodes = async ( { controller } ) => {
+    try {
+      const response = await fetch( "https://anime-api-liart.vercel.app/recent-episodes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json" // Set content type header
+        },
+        signal: controller?.signal
+      } );
 
+      if ( response.ok ) {
+        const body = await response.json();
+        setRecentEpisodes( body );
+      }
+    } catch ( e ) {
+      if ( e.name != "AbortError" ) console.log( e );
+    }
+  };
 
   const fetchTopAiring = async ( { controller } ) => {
     try {
@@ -65,13 +83,15 @@ const AnimePage = () => {
   useEffect( () => {
     const controller = new AbortController();
 
+    fetchRecentEpisodes( { controller: null } );
+
     if ( input.trim().length ) {
 
       fetchAnime( { controller } );
 
     } else {
 
-      fetchTopAiring( { controller } );
+      fetchTopAiring( { controller: null } );
 
     }
 
@@ -108,7 +128,21 @@ const AnimePage = () => {
             <AnimeCardContainer animes={ searchResults.results } className={ Styles[ "anime-container" ] } />
           ) }
         </div>
-        <div className={ Styles[ "recent-episodes" ] }></div>
+        <div className={ Styles[ "recent-episodes" ] }>
+          <p className={ Styles[ "recent-episodes-heading" ] }>
+            Recent Episodes
+          </p>
+          { recentEpisodes?.results.length && recentEpisodes.results.map( ( ep, id ) => (
+            <div className={ Styles[ "episode-container" ] } key={ id }>
+              <img src={ ep.image } alt="" />
+              <div className={ Styles[ "content" ] }>
+                <p className={ Styles[ "title" ] }>{ ep.title.length > 40 ? ep.title.slice( 0, 40 ) + " ..." : ep.title }</p>
+                <p className={ Styles[ "number" ] }>Episode { ep.episodeNumber }</p>
+              </div>
+            </div>
+          ) )
+          }
+        </div>
       </div>
     </>
   );
