94f09d5ca9c94d045ad8a08f400076e180abaf21
Ray <buttrafay980@gmail.com>
2024-02-15 17:26:02 +0500

    updating smoothness

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -5,29 +5,29 @@ import { SocialLionks } from "../../Constants";
 import Loader from "../../Assets/Imgs/loader.gif";
 
 const HomePage = () => {
-  const [loaded, setLoaded] = useState(false);
-  const [oneCycleComplete, setAnimation] = useState(false);
+  const [ loaded, setLoaded ] = useState( false );
+  const [ oneCycleComplete, setAnimation ] = useState( false );
 
-  useEffect(() => {
-    function setStateLoaded() {
-      if (document.readyState == "complete") {
-        setLoaded(true);
-        console.log("loaded");
+  useEffect( () => {
+    function setStateLoaded () {
+      if ( document.readyState == "complete" ) {
+        setLoaded( true );
+        console.log( "loaded" );
       }
     }
 
     setStateLoaded();
 
-    const handleStateChange = (e) => {
+    const handleStateChange = ( e ) => {
       setStateLoaded();
     };
 
-    document.addEventListener("readystatechange", handleStateChange);
+    document.addEventListener( "readystatechange", handleStateChange );
 
     return () => {
-      document.removeEventListener("readystatechange", handleStateChange);
+      document.removeEventListener( "readystatechange", handleStateChange );
     };
-  }, []);
+  }, [] );
 
   let variants = {
     initial: { y: -10, opacity: 0 },
@@ -49,82 +49,82 @@ const HomePage = () => {
   return (
     // <AnimatePresence mode='wait'>
     <>
-      <section className={Styles["home-section"]}>
-        <div className={Styles["overlay"]}></div>
+      <section className={ Styles[ "home-section" ] }>
+        <div className={ Styles[ "overlay" ] }></div>
         <AnimatePresence mode="wait">
-          {!loaded && (
+          { !loaded && (
             <motion.div
-              className={Styles["loader"]}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              onAnimationComplete={() => setAnimation(true)}
+              className={ Styles[ "loader" ] }
+              initial={ { opacity: 0 } }
+              animate={ { opacity: 1 } }
+              exit={ { opacity: 0 } }
+              onAnimationComplete={ () => setAnimation( true ) }
             >
               <img
-                src={Loader}
-                width={200}
-                height={200}
-                className={Styles.player_loader}
+                src={ Loader }
+                width={ 200 }
+                height={ 200 }
+                className={ Styles.player_loader }
               />
             </motion.div>
-          )}
+          ) }
         </AnimatePresence>
-        {loaded && (
+        { loaded && (
           <>
             <motion.div
-              className={Styles["container"]}
-              variants={variants}
+              className={ Styles[ "container" ] }
+              variants={ variants }
               animate="animate"
               initial="initial"
               exit="exit"
-              transition={{ duration: 0.7 }}
+              transition={ { duration: 0.7 } }
             >
               <motion.div
-                className={Styles["hero"]}
-                variants={variants}
-                transition={{ delay: 0.2, duration: 0.7 }}
+                className={ Styles[ "hero" ] }
+                variants={ variants }
+                transition={ { delay: 0.2, duration: 0.7 } }
               >
-                <p className={Styles["p1"]}>Stooge Pictures</p>
+                <p className={ Styles[ "p1" ] }>Stooge Pictures</p>
                 <motion.div
-                  className={Styles["d2"]}
-                  variants={variants}
-                  transition={{ delay: 0.45, duration: 0.7 }}
+                  className={ Styles[ "d2" ] }
+                  variants={ variants }
+                  transition={ { delay: 0.45, duration: 0.7 } }
                 >
-                  <p className={Styles["description"]}>
+                  <p className={ Styles[ "description" ] }>
                     Experience seamless entertainment where you can watch movies
                     and shows, and even download your favorites for offline
                     enjoyment.
                   </p>
-                  <p className={Styles["p2"]}>Feel Free</p>
+                  <p className={ Styles[ "p2" ] }>Feel Free</p>
                 </motion.div>
               </motion.div>
             </motion.div>
             <motion.div
-              className={Styles["links"]}
-              variants={socialLinksVariants}
+              className={ Styles[ "links" ] }
+              variants={ socialLinksVariants }
               animate="animate"
               initial="initial"
-              transition={{ staggerChildren: 0.2, duration: 1 }}
+              transition={ { staggerChildren: 0.2, duration: 1 } }
             >
-              {SocialLionks.map((social, index) => (
+              { SocialLionks.map( ( social, index ) => (
                 <motion.a
-                  href={social.link}
+                  href={ social.link }
                   target="_blank"
-                  className={Styles["link"]}
-                  variants={socialLinksVariants}
-                  whileHover={{ scale: 1.4 }}
-                  key={index}
+                  className={ Styles[ "link" ] }
+                  variants={ socialLinksVariants }
+                  whileHover={ { scale: 1.4 } }
+                  key={ index }
                 >
-                  <img src={social.src} alt="" />
+                  <img src={ social.src } alt="" />
                 </motion.a>
-              ))}
+              ) ) }
             </motion.div>
           </>
-        )}
+        ) }
       </section>
     </>
     // </AnimatePresence>
   );
 };
 
-export default memo(HomePage);
+export default memo( HomePage );
